315301739a95e8cebdb0a1040453e373ef05f495
Adam Herro <adam.herro@comcast.net>
2025-09-13 18:01:41 -0400

    Creating simple calculator. Throws IllegalArgumentException if dividing or using modulo by zero.

---
+++ src/Calculator.java
@@ -0,0 +1,41 @@
+// This is a calculator
+public class Calculator {
+
+    public void invalidInput() {
+        throw new IllegalArgumentException("Invalid Argument: Cannot divide by 0");
+    }
+
+    public int add(int value1, int value2) {
+        return value1 + value2;
+    }
+
+    public int subtract(int value1, int value2) {
+        return value1 - value2;
+    }
+
+    public int multiply(int value1, int value2) {
+        return value1 * value2;
+    }
+
+    public int divide(int value1, int value2) {
+        if(value2 == 0) {
+            invalidInput();
+        }
+        return value1 / value2;
+    }
+
+    public int modulo(int value1, int value2) {
+        if(value2 == 0) {
+            invalidInput();
+        }
+        return value1 % value2;
+    }
+
+    public static void main(String[] args) {
+        Calculator myCalculator = new Calculator();
+
+        System.out.println(myCalculator.add(5, 7));
+        System.out.println(myCalculator.subtract(45, 11));
+        System.out.println(myCalculator.divide(45, 0));
+    }
+}
